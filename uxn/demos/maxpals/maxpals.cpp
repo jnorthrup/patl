80f46b65ed1d41b2f0da2161e39d7df1f89ee417
uxnuxn <uxnuxn@ac5c578c-d34d-0410-9547-55c0b358f148>
2009-02-03 09:12:23 +0000

    found much more simple algorithm for maximal palindromes in string in O(N) time

--- uxn/demos/maxpals/maxpals.cpp
+++ uxn/demos/maxpals/maxpals.cpp
@@ -1,3 +1,7 @@
+// были найдены простые алгоритмы нахождени€ максимальных палиндромов,
+// без построени€ суффиксного дерева и структур дл€ нахождени€ LCA за O(1)
+// http://e-maxx.ru/algo/palindromes_count
+
 #include <uxn/patl/suffix_set.hpp>
 #include <uxn/patl/lca_oracle.hpp>
 #include <uxn/patl/leaf_oracle.hpp>
@@ -12,13 +16,13 @@ int main()
 {
     char pals[1024] =
         "mississippiarozaupalanalapuazoraaaaasobakabosavjfhdlomosmokinggnikomsomolsfsdlfhbvhamanaplanacanalpanamajfgjbf4603974fg4u";
-    const unsigned n = strlen(pals);
+    const int n = strlen(pals);
     strcpy(pals + n + 1, pals);
     _strrev(pals + n + 1);
     //
     SuffixSet sufs(pals, 2 * n + 1);
     printf("building suffix tree...");
-    for (unsigned i = 0; i != 2 * n + 1; ++i)
+    for (int i = 0; i != 2 * n + 1; ++i)
         sufs.push_back();
     printf("done!\n");
     printf("lca preprocessing...");
@@ -29,24 +33,62 @@ int main()
     printf("done!\n");
     //
     printf("---\n");
-    for (unsigned i = 1; i != n; ++i)
+    for (int i = 1; i != n; ++i)
     {
         const vertex v = lca(
             leaf(pals + i),
             leaf(pals + 2 * n + 1 - i));
-        const unsigned q = v.prefix_length() / 8;
+        const int q = v.prefix_length() / 8;
         if (q)
             printf("% 4d %s\n", i, std::string(pals + i - q, 2 * q).c_str());
     }
+    {
+        printf("***\n");
+        std::vector<int> d(n);
+        int l = 0, r = -1;
+        for (int i = 0; i < n; ++i)
+        {
+            int k = (i > r ? 0 : patl::impl::get_min(d[l + r - i + 1], r - i + 1)) + 1;
+            while (i + k - 1 < n && i - k >= 0 && pals[i + k - 1] == pals[i - k])
+                ++k;
+            d[i] = --k;
+            if (i + k - 1 > r)
+            {
+                l = i - k;
+                r = i + k - 1;
+            }
+            if (d[i])
+                printf("% 4d %s\n", i, std::string(pals + i - d[i], 2 * d[i]).c_str());
+        }
+    }
     //
     printf("---\n");
-    for (unsigned i = 1; i != n; ++i)
+    for (int i = 1; i != n; ++i)
     {
         const vertex v = lca(
             leaf(pals + i),
             leaf(pals + 2 * n - i));
-        const unsigned q = v.prefix_length() / 8 - 1;
+        const int q = v.prefix_length() / 8 - 1;
         if (q)
             printf("% 4d %s\n", i, std::string(pals + i - q, 2 * q + 1).c_str());
+    }
+    {
+        printf("***\n");
+        std::vector<int> d(n);
+        int l = 0, r = -1;
+        for (int i = 0; i != n; ++i)
+        {
+            int k = (i > r ? 0 : patl::impl::get_min(d[l + r - i], r - i)) + 1;
+            while (i + k < n && i - k >= 0 && pals[i + k] == pals[i - k])
+                ++k;
+            d[i] = --k;
+            if (i + k > r)
+            {
+                l = i - k;
+                r = i + k;
+            }
+            if (d[i])
+                printf("% 4d %s\n", i, std::string(pals + i - d[i], 2 * d[i] + 1).c_str());
+        }
     }
 }
