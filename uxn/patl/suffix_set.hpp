bfcea0909d65165a1e7c634ab18c3a1045e70f7a
uxnuxn <uxnuxn@ac5c578c-d34d-0410-9547-55c0b358f148>
2009-06-17 11:40:03 +0000

    get rid of patl/impl/(trie_|suffix_)(set|map).hpp; integrate their in patl/*.hpp

--- uxn/patl/suffix_set.hpp
+++ uxn/patl/suffix_set.hpp
@@ -1,7 +1,7 @@
 #ifndef PATL_SUFFIX_SET_HPP
 #define PATL_SUFFIX_SET_HPP
 
-#include "impl/suffix_set.hpp"
+#include "impl/node.hpp"
 #include "impl/suffix_generic.hpp"
 #include "bit_comp.hpp"
 
@@ -9,6 +9,58 @@ namespace uxn
 {
 namespace patl
 {
+namespace impl
+{
+
+template <typename T>
+class node_gen_suffix
+    : public node_generic<node_gen_suffix<T> >
+{
+public:
+#ifdef PATL_DEBUG
+    node_gen_suffix<T>(word_t id)
+        : id_(id)
+    {
+    }
+#endif
+
+    const typename T::value_type &get_value(const typename T::key_type &key) const
+    {
+        return key;
+    }
+
+    typename T::value_type &get_value(const typename T::key_type &key)
+    {
+        return key;
+    }
+
+private:
+#ifdef PATL_DEBUG
+    word_t id_;
+#endif
+};
+
+template <
+    typename Type,
+    int Delta,
+    typename BitComp,
+    typename Allocator>
+class suffix_set_traits
+{
+    typedef suffix_set_traits<Type, Delta, BitComp, Allocator> this_t;
+
+public:
+    static const word_t delta = Delta;
+    typedef Type key_type;
+    typedef Type value_type;
+    typedef BitComp bit_compare;
+    typedef Allocator allocator_type;
+    typedef node_gen_suffix<this_t> node_type;
+    typedef const node_type *const_node_type_ref;
+    typedef node_type *node_type_ref;
+};
+
+} // namespace impl
 
 template <
     typename Type,
--- uxn/patl/trie_map.hpp
+++ uxn/patl/trie_map.hpp
@@ -1,7 +1,6 @@
 #ifndef PATL_TRIE_MAP_HPP
 #define PATL_TRIE_MAP_HPP
 
-#include "impl/trie_map.hpp"
 #include "impl/trie_generic.hpp"
 #include "bit_comp.hpp"
 
@@ -9,6 +8,29 @@ namespace uxn
 {
 namespace patl
 {
+namespace impl
+{
+
+template <
+    typename Key,
+    typename Datum,
+    typename BitComp,
+    typename Allocator>
+class trie_map_traits
+{
+public:
+    typedef Key key_type;
+    typedef std::pair<const Key, Datum> value_type;
+    typedef BitComp bit_compare;
+    typedef Allocator allocator_type;
+
+    static const key_type &ref_key(const value_type &val)
+    {
+        return val.first;
+    }
+};
+
+} // namespace impl
 
 template <
     typename Key,
